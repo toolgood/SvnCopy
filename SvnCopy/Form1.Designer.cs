1f2d0e9ed04149e9a7bc1bd35893ea9cae0f3595
toolgood <at6180686@qq.com>
2016-12-09 20:23:47 +0800

    导出svn版本控制修改后的文件

---
+++ SvnCopy/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace SvnCopy
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "当前文件夹：";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(95, 6);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(244, 21);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "目标文件夹：";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(95, 34);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(192, 21);
+            this.textBox2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(293, 32);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(46, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "浏览";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(104, 61);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(137, 31);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "开始复制";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(12, 95);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(305, 12);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "注意：工具只针对有版本控制的文件，新增文件请添加。";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(351, 116);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Svn修改快速复制";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ SvnCopy/Form1.cs
@@ -0,0 +1,132 @@
+﻿using LibSubWCRev;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace SvnCopy
+{
+    public partial class Form1 : Form
+    {
+        private const string svnCopyFilePath = @"C:\SvnCopySetting.txt";
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.textBox1.Text = Application.StartupPath;
+            var dict = LoadSetting();
+            if (dict.ContainsKey(this.textBox1.Text))
+            {
+                this.textBox2.Text = dict[this.textBox1.Text];
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                this.textBox2.Text = fbd.SelectedPath;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (this.textBox2.Text == "")
+            {
+                MessageBox.Show("请选择目标文件夹。");
+                return;
+            }
+            var srcFolderPath = this.textBox1.Text;
+            var tarFolderPath = this.textBox2.Text;
+            SaveSetting();
+            TryCopyFolder(srcFolderPath, srcFolderPath, tarFolderPath);
+            this.Close();
+        }
+
+        private Dictionary<string, string> LoadSetting()
+        {
+            Dictionary<string, string> dict = new Dictionary<string, string>();
+            if (File.Exists(svnCopyFilePath))
+            {
+                var texts = File.ReadAllLines(svnCopyFilePath);
+                foreach (var text in texts)
+                {
+                    var sp=text.Trim().Split('|');
+                    dict[sp[0]] = sp[1];
+                }
+            }
+            return dict;
+        }
+
+        private void SaveSetting()
+        {
+            var srcFolderPath = this.textBox1.Text;
+            var tarFolderPath = this.textBox2.Text;
+            var dict = LoadSetting();
+            dict[srcFolderPath] = tarFolderPath;
+            StringBuilder sb = new StringBuilder();
+            foreach (var item in dict)
+            {
+                sb.Append(item.Key);
+                sb.Append("|");
+                sb.Append(item.Value);
+                sb.Append("\r\n");
+            }
+            File.WriteAllText(svnCopyFilePath, sb.ToString());
+        }
+        public void TryCopyFolder(string folderPath, string srcFolderPath, string tarFolderPath)
+        {
+            var folders = Directory.GetDirectories(folderPath);
+            foreach (var folder in folders)
+            {
+                TryCopyFolder(folder, srcFolderPath, tarFolderPath);
+            }
+            var files = Directory.GetFiles(folderPath);
+            foreach (var file in files)
+            {
+                TryCopy(file, srcFolderPath, tarFolderPath);
+            }
+        }
+
+        public void TryCopy(string filePath, string srcFolderPath, string tarFolderPath)
+        {
+            //if (Path.GetFileName(filePath)=="SvnCopy.exe")
+            //{
+            //    return;
+            //}
+            if (CanCopy(filePath))
+            {
+                var tarFilePath = tarFolderPath + filePath.Substring(srcFolderPath.Length);
+                Directory.CreateDirectory(Path.GetDirectoryName(tarFilePath));
+                File.Copy(filePath, tarFilePath, true);
+            }
+        }
+
+
+        public bool CanCopy(string filePath)
+        {
+            SubWCRev sub = new SubWCRev();
+            sub.GetWCInfo(filePath, true, true);
+            if (sub.HasUnversioned)
+            {
+                return false;
+            }
+            return sub.HasModifications;// || sub.HasUnversioned;
+        }
+
+
+
+
+    }
+}
